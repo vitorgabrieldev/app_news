3fc3c242b972ed4562b57ebec2d3e13c46c913b8
vitorgabrielgit <vitorgabrieldeoliveiragit@gmail.com>
2022-10-06 19:58:03 -0300

    validation

--- front_end/app/components/validation_form.js
+++ front_end/app/components/validation_form.js
@@ -1,20 +1,29 @@
+document.querySelector('#createAccounts').addEventListener('click', () => {this.validationForm.password();});
+window.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') {this.validationForm.password();};
+});
+
 var validationForm = {
 
-    passwordsRepeat: function() {
+    password: function() {
+        let password1 = document.querySelector('#Password1').value;
+        let password2 = document.querySelector('#repeatPassword').value;
+    
+    
+        if (password1.length >= 8) {
+            if (password1 === password2) {
+                document.querySelector('.msgPassword').style.display = 'none';
+                document.querySelector('#repeatPassword').style.border = '#444341 .01rem solid'
+                
+                window.location.href = "newLogin.php";
 
-        let password1 = document.querySelector('#Password').value;
-        let password2 = document.querySelector('#repeat_password').value;
-
-        setInterval(() => {
-            if(password1 && password2) {
-                if (password1 === password2) {
-                    console.log('As senha estão iguais !')
-                };
+            } else {
+                document.querySelector('.msgPassword').style.display = 'block';
+                document.querySelector('#repeatPassword').style.border = '#f00 .01rem solid'
             };
-        }, 1000);
-
+        } else {
+            alert("A senha deve conter acima de 8 digitos !");
+        };
     },
 
 };
-
-validationForm.passwordsRepeat();
